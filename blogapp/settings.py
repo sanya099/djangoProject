573d5f9bc39fe9c268cb9c7e776b99ba1c0db790
sanya099 <sanya099.com>
2026-04-22 10:17:20 +0530

    fix static files and production setup

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # =========================
 # DEFAULT PRIMARY KEY
